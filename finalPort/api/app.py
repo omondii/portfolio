9544fddce0b3f24aa37e21080313bfb910976092
Brian Omondi <omondiibrian00@gmail.com>
2024-07-25 02:59:53 +0300

    Updated requirements.txt

--- finalPort/api/app.py
+++ finalPort/api/app.py
@@ -1,6 +1,18 @@
-from app import create_app
-import os
+from flask import Flask
+from .config import Config
+"""
+Server creation - Serves my personal portfolio
+"""
 
-application = create_app()
 
-app = application
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # Import and register routes
+    from .routes import bp as routes
+    app.register_blueprint(routes)
+
+    return app
+
+app = create_app()
---
+++ finalPort/api/wsgi.py
@@ -0,0 +1,6 @@
+from app import create_app
+import os
+
+application = create_app()
+
+app = application
