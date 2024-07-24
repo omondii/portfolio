94ab458e6948635773b40b46eef18a5ab46707fb
Brian Omondi <omondiibrian00@gmail.com>
2024-07-25 00:37:13 +0300

    Updated imports

--- finalPort/api/__init__.py
+++ finalPort/api/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 Server creation - Serves my personal portfolio
 """
 
-def create_app(config_class='Config'):
+def create_app(config_class='config'):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
